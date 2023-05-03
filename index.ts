bb6ed811864d26096cf5b79c4dddeaaf9e8a91af
Ruby Wang <ruby.yiwang@gmail.com>
2023-05-03 16:47:06 -0700

    add Typescript marker

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
-// Nothing here. Inklings is being developed in a private repo. 
+// Nothing here. 
+// Inklings is being developed in a private repository. 
 // This file is just to indicate to GitHub that this project is written in Typescript.
+
+// At the time of writing this file,
+// It is approximately 75% Typescript
+// and 25% Vue
+// 
 
 
 
